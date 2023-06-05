3acc268a796c9786f18733e2bf2eb2897673cf60
Jeremy Epstein <jazepstein@gmail.com>
2023-06-05 11:40:08 +1000

    Add lib.ts

---
+++ src/lib.ts
@@ -0,0 +1,5 @@
+import { pgpEncryptS3Object } from "./core";
+import { logger } from "./logging";
+import { Config, config } from "./config";
+
+export { Config, config, logger, pgpEncryptS3Object };
